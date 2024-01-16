366a6c58bb83902409817a0e528b9a9b263632bb
phongho01 <phong.ho@napaglobal.com>
2024-01-16 10:56:53 +0700

    add more code

--- wallet/src/controllers/user.controller.js
+++ wallet/src/controllers/user.controller.js
@@ -1,38 +1,17 @@
-const ethers = require('ethers');
+require('dotenv').config();
 const web3 = require('web3');
-const requestIp = require('request-ip');
 const crypto = require('crypto');
-const fs = require('fs');
 const userService = require('../services/user.service');
+const { encryptData, decryptedData } = require('../utils/crypto');
 
 class UserController {
   async create(req, res) {
-    // const ipInfo = requestIp.getClientIp(req);
-    // if (ipInfo != '::1') {
-    //   const validIp = AUTH0_WHITELIST_IP.findIndex(
-    //     (value) => '::ffff:' + value == ipInfo || value == ipInfo
-    //   );
-    //   if (validIp < 0) {
-    //     res.status(403).send('Unauthorized');
-    //     return;
-    //   }
-    // }
-
     const { user, hash } = req.body;
-    console.log(user, hash);
+
+    const decrypted = decryptedData(hash);
+
     const hashedUserId = crypto.createHash('sha256').update(user).digest('hex');
-
-    const privateKey = fs.readFileSync('private.pem', 'utf8');
-    const buffer = Buffer.from(hash, 'base64');
-    const decrypted = crypto.privateDecrypt(
-      {
-        key: privateKey.toString(),
-        passphrase: process.env.PASSPHRASE,
-      },
-      buffer
-    );
-
-    if (decrypted.toString('utf8') !== hashedUserId) {
+    if (decrypted !== hashedUserId) {
       res.status(403).send('Unauthorized');
       return;
     }
@@ -50,7 +29,7 @@ class UserController {
     const newUser = await userService.createOne({
       uid_auth0: user,
       address: wallet.address,
-      private_key: wallet.privateKey,
+      private_key: encryptData(wallet.privateKey),
       chain: 'eth',
     });
 
---
+++ wallet/src/utils/crypto.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const crypto = require('crypto');
+
+const generateKeyPair = () => {
+  const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    namedCurve: 'secp256k1',
+    publicKeyEncoding: {
+      type: 'spki',
+      format: 'pem',
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'pem',
+      cipher: 'aes-256-cbc',
+      passphrase: process.env.PASSPHRASE,
+    },
+  });
+  fs.writeFileSync('private.pem', privateKey);
+  fs.writeFileSync('public.pem', publicKey);
+  res.send();
+};
+
+const encryptData = (data) => {
+  const publicKey = fs.readFileSync('public.pem', 'utf8');
+  const buffer = Buffer.from(data);
+  const encrypted = crypto.publicEncrypt(publicKey, buffer);
+  return encrypted.toString('base64');
+};
+
+const decryptedData = (data) => {
+  const privateKey = fs.readFileSync('private.pem', 'utf8');
+  const buffer = Buffer.from(hash, 'base64');
+  const decrypted = crypto.privateDecrypt(
+    {
+      key: privateKey.toString(),
+      passphrase: process.env.PASSPHRASE,
+    },
+    buffer
+  );
+
+  return decrypted.toString('utf8');
+};
+
+module.exports = {
+  generateKeyPair,
+  encryptData,
+  decryptedData,
+};
