f3029b9d250148cddd2436c886638c5eb5603d6e
phongho01 <phong.ho@napaglobal.com>
2024-01-16 11:00:21 +0700

    fix

--- wallet/src/models/User.js
+++ wallet/src/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = mongoose.Schema(
   {
     uid_auth0: { type: String, unique: true, index: true },
     address: { type: String, unique: true, index: true },
-    private_key: { type: String, unique: true },
+    private_key: { type: String, require: true },
     chain: { type: String, default: 'eth' },
   },
   {
