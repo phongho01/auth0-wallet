84ca67196c7fc2136c2953ccef3ce8a899ecdb80
phongho01 <phong.ho@napaglobal.com>
2024-01-16 14:04:22 +0700

    add code

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,22 +2,36 @@ import "./App.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const { user, isAuthenticated, logout, loginWithRedirect } = useAuth0();
+  const { user, isAuthenticated, logout, loginWithPopup } = useAuth0();
 
   if (isAuthenticated) {
     console.log(user);
   }
 
   return (
-    <div>
-      <button onClick={() => loginWithRedirect()}>Log In</button>
-      <button
-        onClick={() =>
-          logout({ logoutParams: { returnTo: window.location.origin } })
-        }
-      >
-        Log Out
-      </button>
+    <div className="app">
+      {!isAuthenticated ? (
+        <button onClick={() => loginWithPopup()}>Log In With Auth0</button>
+      ) : (
+        <div className="auth">
+          {
+            <a
+              href={`http://13.213.65.152:3001/api/v1/user/${user.sub}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              User: {user.sub}
+            </a>
+          }
+          <button
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Log Out
+          </button>
+        </div>
+      )}
     </div>
   );
 }
