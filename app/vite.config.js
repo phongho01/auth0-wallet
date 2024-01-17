1a74b0491457a8c7d3bd8c717ed31515b670ffde
phongho01 <phong.ho@napaglobal.com>
2024-01-17 11:44:18 +0700

    add ssl

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
     port: 3000,
+    https: true,
   },
   preview: {
     port: 3000,
+    https: true,
   },
 });
